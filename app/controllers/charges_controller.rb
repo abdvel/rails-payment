05ad5073add6ad630d54a0b8b132f8890eb34802
Abdullah Velioğlu <abdullahvelioglu08@gmail.com>
2016-11-30 18:46:16 +0300

    we can store an display purchase

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,9 +10,14 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => params[:amount],
-      :description => 'Rails Stripe customer',
+      :description => 'Rails Payment Customer',
       :currency    => 'usd'
     )
+    purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken],
+      amount: params[:amount], description: charge.description, currency: charge.currency,
+      customer_id: customer.id, product_id: 1)
+
+    redirect_to purchase
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
