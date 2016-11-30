5b06dd15dbf66b42f8edaa14a503d913894105af
Abdullah Velioğlu <abdullahvelioglu08@gmail.com>
2016-12-01 02:46:52 +0300

    added first product to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+niceCat = Product.create(title: "Quality Kitty",
+  subtitle: "Rails Payment", image_name: "kitty.JPG",
+  price: "4.99", sku: "kittyone", download_url: "https://drive.google.com/file/d/0B_IgSkXhZOoUMXhxdm15OG1OTTQ/view?usp=sharing",
+  details: "A really nice cat", description: %{It's a cat
+  })
